ed1a2d76a47b8a732a20858984ed2c98cb59bcc8
Vincent Wei <vincent@minigui.org>
2023-05-12 16:19:13 +0800

    tune

--- rename-to-my-project.sh
+++ rename-to-my-project.sh
@@ -4,17 +4,15 @@ if echo "$1" | grep -q '^[A-Z][a-z][a-z0-9]*[A-Z][a-z][a-z0-9]*'; then
     ProjectName=$1
     PROJECTNAME=`echo $1| tr "a-z" "A-Z"`
     projectname=`echo $1| tr "A-Z" "a-z"`
-    echo $ProjectName
-    echo $PROJECTNAME
-    echo $projectname
+    echo "Going to rename the project to $ProjectName..."
 else
     echo "Invalid project name; must be one like 'ProjectName', 'MyProject', or 'My1Pt1'."
     exit 1
 fi
 
-sed -i "s/FooBar/$ProjectName/g" `grep FooBar * -rl`
-sed -i "s/FOOBAR/$PROJECTNAME/g" `grep FOOBAR * -rl`
-sed -i "s/foobar/$projectname/g" `grep foobar * -rl`
+sed -i "s/FooBar/$ProjectName/g" `grep FooBar CMakeLists.txt source/ -rl`
+sed -i "s/FOOBAR/$PROJECTNAME/g" `grep FOOBAR CMakeLists.txt source/ -rl`
+sed -i "s/foobar/$projectname/g" `grep foobar CMakeLists.txt source/ -rl`
 
 mv source/cmake/target/FooBar.cmake source/cmake/target/$ProjectName.cmake
 
